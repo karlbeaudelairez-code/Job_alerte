bce5b9ab4758329fb2e0bcbe36e216b0bbe52a1c
Karl Beaude <karlbeaudelairez@gmail.com>
2026-05-17 13:28:51 +0100

    Modification du contenu d'une fonction

--- app.py
+++ app.py
@@ -17,7 +17,10 @@ FICHIER_CANDIDATS = "candidats.json"
 
 
 def get_db_connection():
-    conn = psycopg2.connect(os.getenv('DATABASE_URL'))
+    conn = psycopg2.connect(
+        os.getenv('DATABASE_URL'),
+        sslmode('require')
+    )
     return conn
 
 def init_db():
